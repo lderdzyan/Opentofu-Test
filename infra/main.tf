8b97c03cb29ad81b8e82c9297796f30296fb1690
lderdzyan <lderdzyan@vtgsoftware.com>
2026-01-14 15:48:52 +0400

    Update

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,13 @@
 resource "aws_s3_object" "upload_file" {
     bucket = var.bucket_name
-    key = "${var.folder_name}/${var.file_name}.zip"
-    source = "../lambda/${var.file_name}"
+    key = "dev/${var.file_name}.zip"
+    source = "../lambda/${var.file_name}.zip"
+}
+terraform {
+  backend "s3" {
+    bucket         = var.state_bucket_name
+    key            = "dev/${var.file_name}/terraform.tfstate"   
+    region         = var.region      
+    encrypt        = true
+  }
 }
