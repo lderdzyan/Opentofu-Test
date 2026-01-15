2d20c1b84c835219d46c03338a3abcc2bb348e42
lderdzyan <lderdzyan@vtgsoftware.com>
2026-01-15 14:49:54 +0400

    push to main

--- src/register/index.js
+++ src/register/index.js
@@ -1,7 +1,7 @@
 exports.handler = async ()=> {
     const response = {
        statusCode: 200,
-       body: JSON.stringify('Hello from Lambda2!'),
+       body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
 }
